de25f54fff9c4bf3de3d5e2b79fee61d6ab10fcf
sagarjadhav968 <sagarjadhav968>
2018-06-05 17:47:06 +0530

    Initial commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,10 +5,9 @@ import {Router} from "@angular/router";
   templateUrl: 'home.html'
 })
 export class HomeComponent {
-   menuData:any;	
   constructor(private _router : Router){
-    
- this.menuData=[{"text":"Core","expand":true,"children":[{"text":"Sas","routerLink":"core/sas","selected":false}]}];   }
+
+  }
   routeToLink(data: any){
      if(!data.hasOwnProperty('children') && data.routerLink)
       this._router.navigate([data.routerLink]);
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,5 +1,5 @@
 /**
- * Created by Sagar Jadhav.
+ * Created by sagar on 12/2/18.
  */
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
@@ -12,10 +12,8 @@ import {AmexioWidgetModule} from "amexio-ng-extensions";
 const routes: Routes = [
   { path: '', component:HomeComponent,
     children: [
-          	{
-     	path: 'core/sas', loadChildren: './../pages/kedarpro/core/sas/sas.module#SasModule'
-     	},	
-	    ]
+
+    ]
   }
 ];
 
